7af1c79d4a727a56cc869732d88e0ae7375abb3b
sam-morin <sammorin@live.com>
2023-10-08 08:27:01 -0400

    update uninstall script

--- uninstall-nottbox.sh
+++ uninstall-nottbox.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
-echo "Uninstalling Nottbox..."
-
 # Stop the Nottbox service if it's running
+echo ""
+echo "Stopping the Nottbox service..."
 systemctl stop nottbox
 
 # Disable the Nottbox service from starting on boot if it's enabled
+echo ""
+echo "Disabling the Nottbox service..."
 systemctl disable nottbox
 
 # Remove the Nottbox service unit file
+echo ""
+echo "Deleting the Nottbox service..."
 rm /etc/systemd/system/nottbox.service
 
 # Reload systemd daemon after removing the service file
+echo ""
+echo "Reloading systemd daemomn..."
 systemctl daemon-reload
 
 # Remove the Nottbox directory
+echo ""
+echo "Removing the Nottbox directory..."
 rm -rf /root/nottbox
 
 # Uninstall the git package
+echo ""
+echo "Uninstalling git..."
 apt-get remove --purge git -y
 
 # Clean up unused packages and dependencies (optional)
+echo ""
+echo "Cleaning up packages..."
 apt-get autoremove -y
 
-echo "Nottbox has been uninstalled."
+echo ""
+echo "Nottbox has been uninstalled, and the git package has been removed."
 
-# Self-destruct this script
-rm -- "$0"
+# Schedule self-destruct using 'at' command
+echo "rm -- \"\$0\"" | at now + 15 seconds
 
+echo ""
+echo "This uninstall script will self-destruct in 10 seconds."
+echo ""
+echo "Thank you for using Nottbox! - https://github.com/sam-morin/nottbox"
+echo ""
+
+# Exit the script gracefully
+exit 0
