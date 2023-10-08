018cd4cec3fea4c14710dfb3adea568c05aa6dbf
sam-morin <sammorin@live.com>
2023-10-08 07:25:03 -0400

    add install script

--- install-nottbox.sh
+++ install-nottbox.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Install git without prompting
-sudo apt-get -y install git
-
 # Prompt the user to confirm if they want to enable the service
+echo ""
 read -p "Do you want to enable the Nottbox service to start on boot? (Y/n): " enable_service
 
 if [ "$enable_service" != "n" ]; then
+    # Install git without prompting
+    sudo apt-get -y install git
+
     # Clone the repository
     git clone https://github.com/sam-morin/nottbox.git
 
@@ -20,12 +21,14 @@ if [ "$enable_service" != "n" ]; then
     sudo cp nottbox.service /etc/systemd/system/
 
     # Prompt the user to edit the YAML file using vi
+    echo ""
     read -p "Do you want to edit the Nottbox default config file? (Y/n): " edit_yaml
 
     if [ "$edit_yaml" != "n" ]; then
         sudo vi /root/nottbox/nottbox.yml
     else
-        echo "You chose not to edit the YAML file."
+        echo ""
+        echo "You chose not to edit the default Nottbox YAML config file!"
     fi
 
     # Reload systemd daemon to pick up the new service
@@ -40,5 +43,6 @@ if [ "$enable_service" != "n" ]; then
     # Check the status of the service
     sudo systemctl status nottbox
 else
-    echo "Service 'nottbox' will not be enabled to start on boot."
+    echo ""
+    echo "WARN: Nottbox service will not be enabled to start on boot. This is probably not a good idea as Nottbox will only be able to do it's job once!"
 fi
